9639a586ba86cd1274ac14bb777698dfc173e0b8
Volodymyr <vivk2@ukr.net>
2018-07-03 14:13:27 +0300

    added position

---
+++ src/main/java/com/dibrova/entity/Position.java
@@ -0,0 +1,27 @@
+package com.dibrova.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="POSITIONTYPE")
+public class Position  extends AbstractModelClass {
+
+    @Column(name="position")
+    String position;
+
+    @OneToOne
+    @JoinColumn(name = "userid",
+            foreignKey = @ForeignKey(name = "USERID"))
+    @JsonBackReference
+    private User user;
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+}
